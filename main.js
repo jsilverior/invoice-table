81b72d4f671e4acde6effbd317be70742bb3de75
Jonathan <jsilveri@pichincha.com>
2022-10-11 07:44:33 -0400

    Adds features

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
+class GenericEditorAsRenderer {
+  init(params) {
+    this.inputField = document.createElement("input");
+    this.inputField.value = params.value;
+    this.inputField.className = ".ag-theme-alpine .ag-text-field-input";
+    this.inputField.onchange = () => params.setValue(this.inputField.value);
+  }
+
+  getGui() {
+    return this.inputField;
+  }
+
+  refresh(params) {
+    return false;
+  }
+}
+
 const columnDefs = [
   {
     field: "lineItem",
     headerName: "List Item",
   },
-  { field: "service", headerName: "Service Description" },
+  { field: "service", headerName: "Service Description", resizable: true },
   {
     field: "po_detail_amount",
     headerName: "PO Detail Amount",
@@ -19,28 +36,38 @@ const columnDefs = [
   {
     field: "invoice_detail_cost",
     headerName: "Invoice Detail Cost",
-    editable: true,
+    cellStyle: {
+      color: "white",
+      "background-color": "#4caf50",
+      padding: "0 4px",
+    },
+    cellRenderer: GenericEditorAsRenderer,
   },
   {
     field: "valid_detail_cost",
     headerName: "Valid Detail Cost",
-    editable: true,
+    cellStyle: {
+      color: "white",
+      "background-color": "#4caf50",
+      padding: "0 4px",
+    },
+    cellRenderer: GenericEditorAsRenderer,
   },
 ];
 
 // specify the data
 const rowData = [
   {
-    lineItem: 123456,
+    lineItem: 123451,
     service: "Description 1",
     po_detail_amount: "10,000",
-    invoice_detail_amount: "0",
+    invoice_detail_amount: "1",
     available_amount: "10000",
     invoice_detail_cost: "1000",
     valid_detail_cost: "1000",
   },
   {
-    lineItem: 123456,
+    lineItem: 123452,
     service: "Description 1",
     po_detail_amount: "10,000",
     invoice_detail_amount: "0",
@@ -99,6 +126,13 @@ const rowData = [
 const gridOptions = {
   columnDefs: columnDefs,
   rowData: rowData,
+  singleClickEdit: true,
+  defaultColDef: {
+    resizable: true,
+    sortable: true,
+    filter: true,
+  },
+  multiSortKey: "ctrl",
 };
 
 // setup the grid after the page has finished loading
